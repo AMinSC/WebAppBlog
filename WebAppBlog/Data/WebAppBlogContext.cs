e482e83ae77780a394f6ed55d6f2aa4f238ffd47
AMinSC <cdtjdcjf@gmail.com>
2025-06-12 21:28:20 +0900

    Apply scaffolds

---
+++ WebAppBlog/Data/WebAppBlogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppBlog.Models;
+
+namespace WebAppBlog.Data
+{
+    public class WebAppBlogContext : DbContext
+    {
+        public WebAppBlogContext (DbContextOptions<WebAppBlogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppBlog.Models.Movie> Movie { get; set; } = default!;
+    }
+}
--- WebAppBlog/Program.cs
+++ WebAppBlog/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppBlog.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppBlogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppBlogContext") ?? throw new InvalidOperationException("Connection string 'WebAppBlogContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
