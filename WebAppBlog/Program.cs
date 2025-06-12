eb7208c8f9e5cc638c4e4986dd2a6b3ee9c8299e
AMinSC <cdtjdcjf@gmail.com>
2025-06-12 22:15:54 +0900

    Data Seeding

--- WebAppBlog/Program.cs
+++ WebAppBlog/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebAppBlog.Data;
+using WebAppBlog.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WebAppBlogContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppBlogContext") ?? throw new InvalidOperationException("Connection string 'WebAppBlogContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<WebAppBlogContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeeData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
