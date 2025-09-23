3fbb008b331b86e0f39246e53ba471bdc8858dfd
AMinSC <cdtjdcjf@gmail.com>
2025-09-23 22:15:33 +0900

    Add DAL Categories

---
+++ WebAppBlog/Data/DAL/CategoriesRepository.cs
@@ -0,0 +1,46 @@
+﻿using WebAppBlog.Models;
+
+namespace WebAppBlog.Data.DAL
+{
+    public class CategoriesRepository
+    {
+        private readonly WebAppBlogContext _context;
+
+        public CategoriesRepository(WebAppBlogContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categories> GetAllCategories()
+        {
+            return _context.Categories.ToList();
+        }
+        
+        public Categories? GetCategoryById(int categoryId)
+        {
+            return _context.Categories.Find(categoryId);
+        }
+
+        public void InsertCategory(Categories category)
+        {
+            _context.Categories.Add(category);
+            Save();
+        }
+
+        public void UpdateCategory(Categories category)
+        {
+            _context.Categories.Update(category);
+            Save();
+        }
+
+        public void DeleteCategory(Categories category)
+        {
+            _context.Categories.Remove(category);
+            Save();
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
